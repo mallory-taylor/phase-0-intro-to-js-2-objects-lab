4c994f1e256ae96ea529a5cc987348b0288fdc7d
Mallory Taylor <malloryataylor13@gmail.com>
2023-03-09 00:36:57 -0500

    Modifying objects

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Mallory",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+const updatedEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "address",
+    "12 Broadway"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+const newName = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Mal");
+
+function deleteFromEmployeeByKey(employee, key) {
+ const deleteName = {...employee};
+ delete deleteName["name"];
+return deleteName;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee["name"];
+    return employee;
+}
